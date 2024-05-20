1cdc74987c840ea000464769d7a74e7c1e211df8
tanishparashar <parashartanish22@gmail.com>
2024-05-20 08:23:20 +0530

    ui changed again

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,30 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-        
-          "primary": "#1553ff",
-          
-          "secondary": "#00ff8c",
-                   
-          "accent": "#00b9ff",
-                   
-          "neutral": "#00060d",
-                   
-          "base-100": "#f0fdff",
-                   
-          "info": "#00e5ff",
-                   
-          "success": "#00f896",
-                   
-          "warning": "#ec0000",
-                   
-          "error": "#ff7d8a",
-        },
-      },
-    ],
+    
   },
    plugins: [require("daisyui")],
 }
